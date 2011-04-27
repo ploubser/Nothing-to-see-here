7b2c39ed9439ea5195eef5e98bfe9f9f6c6b5f42
Pieter Loubser <ploubser@gmail.com>
2011-04-27 11:40:40 +0100

    Rasterize lib part 2

--- rasterize.c
+++ rasterize.c
@@ -1,40 +1,4 @@
-#ifndef RASTERIZE_H
-#define RATSERIZE_H
-
-#include "stdio.h"
-#include "stdlib.h"
-#include "stdint.h"
-
-#pragma pack(push,1)
-
-typedef uint16_t WORD;
-typedef uint32_t DWORD;
-
-typedef struct tagBITMAPFILEHEADER
-{
-    WORD bfType;
-    DWORD bfSize;
-    WORD bfReserved1;
-    WORD bfReserved2;
-    DWORD bfOffBits;
-} BITMAPFILEHEADER;
-
-typedef struct tagBITMAPINFOHEADER
-{
-    DWORD  biSize; 
-    DWORD  biWidth; 
-    DWORD  biHeight; 
-    WORD   biPlanes; 
-    WORD   biBitCount; 
-    DWORD  biCompression; 
-    DWORD  biSizeImage; 
-    DWORD  biXPelsPerMeter; 
-    DWORD  biYPelsPerMeter; 
-    DWORD  biClrUsed; 
-    DWORD  biClrImportant; 
-} BITMAPINFOHEADER;
-
-void toMatrix(char *);
+#include "rasterize.h"
 
 void toMatrix(char *Filename)
 {
@@ -95,5 +59,3 @@ void toMatrix(char *Filename)
     }
 
 }
-
-#endif
